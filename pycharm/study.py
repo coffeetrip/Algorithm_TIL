eed80e7cde8a08bd5469f4bbdc9f8118e358e14d
양예은 <yye7926@naver.com>
2019-12-09 01:31:01 +0900

    20191209

--- pycharm/study.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.stdin = open('input.txt', 'r')
-
-
-a = (int, input().split(','))
-a1 = 1
-a2 = 1
-for i in range(1, a+1, 1):
--- pycharm/회전_양예은.py
+++
@@ -1,8 +0,0 @@
-for tc in range(int(input())):
-    n, m = map(int, input().split())
-    num = list(map(int, input().split()))
-
-    while m > 0:
-        num.append(num.pop(0))
-        m -= 1
-    print(f'#{tc+1} {num[0]}')
